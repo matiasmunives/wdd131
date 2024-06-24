d8afc887d0d80d42c70346fdd5cfef272fbb8b7f
matiasmunives <mun19005@byui.edu>
2024-06-23 23:53:43 -0300

    correction

--- scripts/temples.js
+++ scripts/temples.js
@@ -16,3 +16,17 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const formattedTime = lastModified.toLocaleTimeString('en-US', timeOptions);
     lastUpdatedElement.innerText = `${formattedDate} ${formattedTime}`;
   });
+  
+  //Hamburguer - Menu 
+  const hamburger = document.querySelector(".hamburger-menu");
+  const nav_mobile = document.querySelector(".nav-mobile");
+  const backdrop = document.querySelector(".backdrop")
+  
+  const handleMobileNav =()=>{
+      hamburger.classList.toggle("transform-menu")
+      nav_mobile.classList.toggle("show-nav-mobile")
+      backdrop.classList.toggle("show-backdrop")
+  }
+  
+  hamburger.addEventListener("click",handleMobileNav)
+  backdrop.addEventListener("click",handleMobileNav)
